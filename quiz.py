107626f4052ab571f2fdfec1e4294aa7bcf985d9
ZK Pang <zhenkangpang@gmail.com>
2017-09-15 10:38:09 -0400

    test: Update test cases

--- quiz.py
+++ quiz.py
@@ -83,3 +83,21 @@ print("# 4: Orthogonal - {}".format(myVector1.isOrthogonal(myVector2)))
 
 print("Quiz: Projecting Vectors")
 # 1
+myVector1 = Vector([3.039, 1.879])
+myVector2 = Vector([0.825, 2.036])
+print("# 1: Parallel Projection")
+print(myVector1.parallelProjection(myVector2))
+
+# 2
+myVector1 = Vector([-9.88, -3.264, -8.159])
+myVector2 = Vector([-2.155, -9.353, -9.473])
+print("# 2: Orthogonal Projection")
+print(myVector1.orthogonalProjection(myVector2))
+
+# 3
+myVector1 = Vector([3.009, -6.172, 3.692, -2.51])
+myVector2 = Vector([6.404, -9.144, 2.759, 8.718])
+print("# 3: Parallel Projection")
+print(myVector1.parallelProjection(myVector2))
+print("# 3: Orthogonal Projection")
+print(myVector1.orthogonalProjection(myVector2))
