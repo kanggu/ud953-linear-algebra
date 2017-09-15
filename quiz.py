0eafb2dbf89c7d820b5a2f6fd61f9a4c36ed0b29
ZK Pang <zhenkangpang@gmail.com>
2017-09-15 13:11:40 -0400

    test: Update test cases

--- quiz.py
+++ quiz.py
@@ -101,3 +101,20 @@ print("# 3: Parallel Projection")
 print(myVector1.parallelProjection(myVector2))
 print("# 3: Orthogonal Projection")
 print(myVector1.orthogonalProjection(myVector2))
+
+
+print("Quiz: Cross Product")
+# 1
+myVector1 = Vector([8.462, 7.893, -8.187])
+myVector2 = Vector([6.984, -5.975, 4.778])
+print("# 1: ", myVector1.cross(myVector2))
+
+# 2
+myVector1 = Vector([-8.987, -9.838, 5.031])
+myVector2 = Vector([-4.268, -1.861, -8.866])
+print("# 2: Parallelogram area - {}".format(myVector1.cross(myVector2).magnitude()))
+
+# 3
+myVector1 = Vector([1.5, 9.547, 3.691])
+myVector2 = Vector([-6.007, 0.124, 5.772])
+print("# 3: Triangle area - {}".format(myVector1.cross(myVector2).magnitude() / 2))
