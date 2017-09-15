12cfd2c7672b1d303ebb3b3186bd0e5dc525b797
ZK Pang <zhenkangpang@gmail.com>
2017-09-15 10:15:12 -0400

    doc: Cleanup of codes

--- Vector.py
+++ Vector.py
@@ -7,14 +7,12 @@ class Vector(object):
                 raise ValueError
             self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
-            self.magnitude = math.sqrt(sum([i ** 2 for i in coordinates]))
 
         except ValueError:
             raise ValueError("The coordinates must be nonempty")
 
         except TypeError:
             raise TypeError("The coordinates must be an iterable")
-
 
     def __str__(self):
         return "Vector: {}".format(self.coordinates)
@@ -30,48 +28,47 @@ class Vector(object):
 
     def __mul__(self, x):
         if type(x) == type(self):
-            return self.inner(x)
+            return self.dot(x)
         else:
             return Vector([x * i for i in self.coordinates])
 
     def __rmul__(self, n):
         return self.__mul__(n)
 
+    def magnitude(self):
+        return math.sqrt(sum([i ** 2 for i in self.coordinates]))
+
     def __truediv__(self, n):
         return Vector([i / n for i in self.coordinates])
 
-    def direction(self): # unit vector
+    def direction(self): # unit vector, normalized vector
         try:
-            if self.isZero():
-                raise ZeroVectorError
-            else:
-                return self / self.magnitude
-        except ZeroVectorError:
-            raise ZeroVectorError("This function is not valid with zero vector.")
+            return self / self.magnitude()
+        except ZeroDivisionError:
+            raise Exception("Cannot normalize the zero vector.")
 
-    def inner(self, v):
+    def dot(self, v):
         return sum([i * j for i,j in zip(self.coordinates,v.coordinates)])
 
-    def angle(self, v):
+    def angle(self, v, inDegree=False):
         try:
             if self.isZero() or v.isZero():
                 raise ZeroVectorError
             else:
-                return math.acos(self * v / self.magnitude / v.magnitude)
+                angle = math.acos(self.direction() * v.direction())
+                if inDegree:
+                    return angle * 180 / math.pi
+                else:
+                    return angle
         except ZeroVectorError:
             raise ZeroVectorError("This function is not valid with zero vector.")
 
-    def isZero(self, tolerance = 1e-10):
-        return self.magnitude < tolerance
+    def isZero(self, tolerance = 1e-10): # if is zero vector
+        return self.magnitude() < tolerance
 
     def isParallel(self, v):
         return (self.isZero() or v.isZero() or
                 self.angle(v) == 0 or self.angle(v) == math.pi)
 
     def isOrthogonal(self, v, tolerance=1e-10):
-        return self * v < tolerance
-
-myVector1 = Vector([-2.328, -7.284, -1.214])
-myVector2 = Vector([-1.821, 1.072, -2.94])
-print(myVector1.isOrthogonal(myVector2))
-print(myVector1 * myVector2)
+        return abs(self * v) < tolerance
